5fd7b834bce7edc20b884dc9f50bed8d5afe5617
Troy Dutton <thesneakycrafter@gmail.com>
2022-10-15 14:15:52 -0500

    Training & Testing

--- model.py
+++ model.py
@@ -10,10 +10,87 @@ EPOCHS = 10
 IMG_WIDTH = 28
 IMG_HEIGHT = 28
 NUM_CATEGORIES = 26
+TEST_DATA_PATH = "data\\sign_mnist_test.csv"
+TRAIN_DATA_PATH = "data\\sign_mnist_train.csv"
 
 
 def main():
-    pass
+    # Get image arrays and labels for training
+    training_images, training_labels = load_data(TRAIN_DATA_PATH)
+
+    # Split data into training and testing sets
+    training_labels = tf.keras.utils.to_categorical(training_labels)
+
+    #x_train, x_test, y_train, y_test = train_test_split(
+    #    np.array(images), np.array(labels), test_size=1
+    #)
+
+    # Get the model
+    model = get_model()
+
+    # Fit model on training data
+    model.fit(training_images, training_labels, epochs=EPOCHS)
+
+    # Get image arrays and labels for testing
+    testing_images, testing_labels = load_data(TEST_DATA_PATH)
+
+    testing_labels = tf.keras.utils.to_categorical(testing_labels)
+
+    # Evaluate neural network performance
+    model.evaluate(testing_images,  testing_labels, verbose=2)
+
+    # Save model to file
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+        model.save(filename)
+        print(f"Model saved to {filename}.")
+
+def get_model():
+    """
+    Returns a compiled model. Assume that the
+    'input_shape' is (IMG_WIDTH, IMG_HEIGHT, 1)
+    The output layer should have `NUM_CATEGORIES`
+    """
+
+    model = tf.keras.models.Sequential(
+        [   
+            #Convolutional layer
+            tf.keras.layers.Conv2D(16, (3, 3), input_shape=(IMG_WIDTH, IMG_HEIGHT, 1), activation = "relu"),
+
+            #Poolin
+            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+            #Convolutional layer
+            tf.keras.layers.Conv2D(32, (3, 3), activation = "relu"),
+
+            #Poolin
+            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+            #Convolutional layer
+            tf.keras.layers.Conv2D(64, (3, 3), activation = "relu"),
+
+            #Poolin
+            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+            #Flat as a pancake
+            tf.keras.layers.Flatten(),
+
+            #Hidden layer with 128 nodes, ReLU activation
+            tf.keras.layers.Dense(128, activation = "relu"),
+            tf.keras.layers.Dropout(.25),
+
+            #Hidden layer with 128 nodes, ReLU activation
+            tf.keras.layers.Dense(128, activation = "relu"),
+            tf.keras.layers.Dropout(.25),
+
+            #Output layer
+            tf.keras.layers.Dense(NUM_CATEGORIES, activation = "softmax")
+        ]
+    )
+
+    #Compile and return the model
+    model.compile(optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001, decay = 0.0005), loss = "categorical_crossentropy", metrics = ["accuracy"])
+    return model
 
 
 def load_data(data_dir):
@@ -25,22 +102,7 @@ def load_data(data_dir):
     be a list of integer labels, representing the categories for each of the
     corresponding 'images'.
     """
-    images = []
-    labels = []
-    for i in range(NUM_CATEGORIES):
-        #sub_dir Ex: gtsrb\21
-        os.system('cls')
-        print(f"Accessing subdirectory {i+1}/{NUM_CATEGORIES}")
-        sub_dir = os.path.join(data_dir, str(i))
-        for filename in os.listdir(sub_dir):
-            #Load the image as a 'numpy.ndarry'
-            img = cv2.imread(os.path.join(sub_dir, filename))
-            #Resize the image
-            img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT))
-            #Add the image and its label to the data lists
-            images.append(img)
-            labels.append(i)
-    return (images, labels)
+    pass
 
 
 if __name__ == "__main__":
