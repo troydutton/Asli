bcba8aa9f7d2a22f9cb1692dc7a438c95f2fe05e
Bushvacka <45671229+Bushvacka@users.noreply.github.com>
2022-10-25 00:30:56 -0500

    New dataset

--- alphabet_test.py
+++ alphabet_test.py
@@ -1,25 +1,21 @@
 import tensorflow as tf
 import cv2
-import string
-import numpy as np
+import os, string
 
 from main import ALPHABET
 
 EXIT_KEY = 27
+IMG_WIDTH = 200
+IMG_HEIGHT = 200
 ALPHABET = list(string.ascii_lowercase)
+DATA_DIR = "data\\asl_alphabet_test\\asl_alphabet_test"
 
-model = tf.keras.models.load_model("model.h5")
+model = tf.keras.models.load_model("new_model.h5")
 
 
-img = cv2.imread("data\\amer_sign3.png", cv2.IMREAD_GRAYSCALE)
-
-# Runs through the alphabet in sign language   
-for i in range(4):
-    for j in range(6):
-        sub_img = img[(i * 83):(i*83)+83, (j * 83):(j*83)+83]
-        print(ALPHABET[model.predict(np.array(cv2.resize(sub_img, (28, 28))).reshape((-1, 28, 28, 1)).astype(np.uint8)).argmax()])
-        cv2.imshow("Image", sub_img)
-        if (cv2.waitKey(1000) & 0xFF == EXIT_KEY):
-            cv2.destroyAllWindows()
-            break
+for filename in os.listdir(DATA_DIR):
+    img = cv2.imread(os.path.join(DATA_DIR, filename))
+    #Resize the image
+    img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT))
+    print(ALPHABET[model.predict(img.reshape(-1, 200, 200, 3)).argmax()])
 
